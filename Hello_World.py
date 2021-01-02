0c2ff719f9117241135fe910031ab110c92b14c4
Harsh Jain <71202227+HarshJainofficial@users.noreply.github.com>
2021-01-02 21:26:55 +0530

    this change is for Checking updating status on github

--- Hello_World.py
+++ Hello_World.py
@@ -2,3 +2,10 @@ for i in range(20):
     print(i)
 
 print(list(range(20)))
+print(list(range(30)))
+
+
+for i in range(79000):
+    print(i)
+    print('this code specially for checking processing power of processor')
+    print('revising Basic Stuff')
